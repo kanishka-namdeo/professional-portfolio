ee75af1b48235038efa15df7a6fdae38046c694f
kane111 <kanishkanamdeo@hotmail.com>
2026-01-03 16:12:39 +0400

    changes to dpeloy on github pages

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
-const path = require('path');
-const os = require('os');
+const basePath = process.env.NEXT_BASE_PATH || '';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath,
+  assetPrefix: basePath ? `${basePath}/` : '',
   images: {
     unoptimized: true,
-  }
+  },
 };
 
 module.exports = nextConfig;
